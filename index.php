ea634e075b1a514651f8406af37cc1f92db10610
chillyboy07 <bonneviechilly@gmail.com>
2025-05-08 15:18:18 +0800

    feat (landing page): added structure and description for landing page

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gym Day Generator</title>
+<link rel="stylesheet" href="assets/css/example.css">
+</head>
+<body>
+<h1>Chilly's Gym Day Generator 💪</h1>
+
+
+<div class="welcome-text">
+<h2>Ready to Get BIG and STRONG?</h2>
+<p>Welcome to the Gym Day Generator! This website gives you a simple gym routine every time you visit. Whether it's Chest Day, Leg Day, or Rest Day — we've got something for you!</p>
+<a href="page/index.php" class="btn">Start Now</a>
+</div>
+
+</body>
+</html>
